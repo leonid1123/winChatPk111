cbaa7dbb76aaf770e7642d237dd65359ee30d37c
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-04-26 10:37:41 +0300

    comments

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -29,19 +29,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 29;
-            this.listBox1.Location = new System.Drawing.Point(35, 28);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(551, 294);
-            this.listBox1.TabIndex = 0;
             // 
             // timer1
             // 
@@ -49,22 +43,60 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.vScrollBar1);
+            this.panel1.Location = new System.Drawing.Point(27, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(439, 388);
+            this.panel1.TabIndex = 0;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(418, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(21, 388);
+            this.vScrollBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(580, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -13,6 +13,7 @@ namespace winChatPk111
     public partial class Form3 : Form
     {
         List<String> strings = new List<String>();
+        int x = 0;
         public Form3()
         {
             InitializeComponent();
@@ -23,18 +24,28 @@ namespace winChatPk111
             strings.Add("element1");
             strings.Add("element2");
             strings.Add("element3");
-         
-            listBox1.DataSource = strings;
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            strings.Add("element_new");
-            listBox1.BeginUpdate();
-            listBox1.DataSource = null;
-            listBox1.DataSource = strings;
-            listBox1.EndUpdate();
-            Console.WriteLine(strings.Count);
+            int y = 0;
+            foreach (String s in strings)
+            {
+                Label label = new Label();
+                label.Location = new Point(0, y += 25);
+                label.Text = s;
+                panel1.Controls.Add(label);
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            strings.Add(textBox1.Text);
+            textBox1.Text = "";
         }
     }
 }
